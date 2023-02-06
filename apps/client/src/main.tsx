af1abb282acb1735aa879c3fd3f8b72ebb7a7359
Ilya Kazakou <pouutewqer@gmail.com>
2023-02-06 20:56:49 +0300

    feat: configure basic packages
    - Add MUI
    - Add RTK
    - Add Auth0

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './app/app';
 
@@ -10,7 +13,17 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <Auth0Provider
+          domain="YOUR_AUTH0_DOMAIN"
+          clientId="YOUR_AUTH0_CLIENT_ID"
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
+        >
+          <App />
+        </Auth0Provider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
