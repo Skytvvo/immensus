3c0ec5af2268512bfdc39a031bd63adbb1458899
Ilya Kazakou <pouutewqer@gmail.com>
2023-02-06 19:21:05 +0300

    feat: configure storybook
    - Install storybook
    - Configure stories
    - Adjust script commands at package.json

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  'apps/{client,server}/**/*.{ts,tsx}': [
+  'apps/{client,server}/**/*.{js,ts,jsx,tsx}': [
     'nx affected --target lint --uncommitted --fix true',
     'nx affected --target test --uncommitted',
+    'nx affected --target storybook-build --uncommitted',
     'nx affected --target build --uncommitted',
     'nx format:write  --uncommitted',
   ],
