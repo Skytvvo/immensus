19d4b0e1c535e2b1b2a1de34fa7b1ceb528c1ded
Ilya Kazakou <ilya.kazakou@intexsoft.by>
2023-03-17 11:49:28 +0300

    CD: Add Docker - Updated CI/CD workflow - Added dockerfile for client side - Added docker-compose.yml - Added dockerfile for server side - Added & altered js scripts - Added dockerhub to pipeline

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -26,20 +26,21 @@ export function App() {
       <Routes>
         <Route
           path="/"
-          element={
+          element={(
             <div>
-              This is the generated root route.{' '}
+              This is the generated root route.
+              {' '}
               <Link to="/page-2">Click here for page 2.</Link>
             </div>
-          }
+          )}
         />
         <Route
           path="/page-2"
-          element={
+          element={(
             <div>
               <Link to="/">Click here to go back to root page.</Link>
             </div>
-          }
+          )}
         />
       </Routes>
       {/* END: routes */}
