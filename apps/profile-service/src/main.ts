01031c894080d6951e186e2e21d38ff139cec898
Ilya Kazakou <ilya.kazakou@intexsoft.by>
2023-04-03 14:21:08 +0300

    I11: Init Profile service - Created the nx app - Edited the config - Configured docker

--- apps/profile-service/src/main.ts
+++ apps/profile-service/src/main.ts
@@ -3,8 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import cookieParser from 'cookie-parser';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -13,9 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
-  const port = process.env.PORT_AUTHORIZATION_SERVICE || 4400;
+  const port = process.env.PORT_PROFILE_SERVICE || 4402;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
