6d8257f39018c664a1d04c5cf9ff544f34bfe7c5
Ilya Kazakou <ilya.kazakou@intexsoft.by>
2023-04-03 13:55:17 +0300

    I15: Configure Microservices Gateway

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -3,20 +3,28 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT_GATEWAY_SERVICE || 3334;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  // const app = await NestFactory.create(AppModule);
+  // const globalPrefix = 'api';
+  // app.setGlobalPrefix(globalPrefix);
+  // const port = process.env.PORT_GATEWAY_SERVICE || 3334;
+  // await app.listen(port);
+  // Logger.log(
+  //   `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  // );
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
   );
+  await app.listen();
 }
 
 bootstrap();
